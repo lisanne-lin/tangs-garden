8062b909e1831bc3c74f9f583b9d6f44420de333
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2022-04-13 22:12:04 +0200

    removed dark mode from the input form

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -96,7 +96,7 @@
                 @if ($menu_items->count())
                     <div class="relative overflow-x-auto rounded-lg mt-12 border border-gray-200">
                         <table class="w-full text-sm text-left text-gray-500 border">
-                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                                 <tr>
                                     <th scope="col" class="px-6 py-3">
                                         #
@@ -123,9 +123,9 @@
                             </thead>
                             <tbody>
                                 @foreach ($menu_items as $menu_item)
-                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <tr class="bg-white border-b">
                                         <th scope="row"
-                                            class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                             {{ $menu_item->number }}{{ $menu_item->letter }}
                                         </th>
                                         <td class="px-6 py-4">
@@ -153,7 +153,7 @@
                                             <form data-form-id="{{ $menu_item->id }}" action="{{ route('menu/', $menu_item) }}" method="POST">
                                                 @csrf
                                                 @method('DELETE')
-                                                <button type="button" onclick="confirmDelete({{ $menu_item->id }})" class="font-medium text-red-600 dark:text-red-500 hover:underline"><svg xmlns="http://www.w3.org/2000/svg" viewBox="-3 -2 24 24" width="24" fill="currentColor"><path d="M6 2V1a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1h4a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-.133l-.68 10.2a3 3 0 0 1-2.993 2.8H5.826a3 3 0 0 1-2.993-2.796L2.137 7H2a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h4zm10 2H2v1h14V4zM4.141 7l.687 10.068a1 1 0 0 0 .998.932h6.368a1 1 0 0 0 .998-.934L13.862 7h-9.72zM7 8a1 1 0 0 1 1 1v7a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v7a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1z"></path></svg></button>
+                                                <button type="button" onclick="confirmDelete({{ $menu_item->id }})" class="font-medium text-red-600 hover:underline"><svg xmlns="http://www.w3.org/2000/svg" viewBox="-3 -2 24 24" width="24" fill="currentColor"><path d="M6 2V1a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1h4a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-.133l-.68 10.2a3 3 0 0 1-2.993 2.8H5.826a3 3 0 0 1-2.993-2.796L2.137 7H2a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h4zm10 2H2v1h14V4zM4.141 7l.687 10.068a1 1 0 0 0 .998.932h6.368a1 1 0 0 0 .998-.934L13.862 7h-9.72zM7 8a1 1 0 0 1 1 1v7a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v7a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1z"></path></svg></button>
                                             </form>
                                         </td>
                                     </tr>
