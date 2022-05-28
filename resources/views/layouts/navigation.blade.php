3bb63d8effaa0dcda5ba41cb0319c4e5a238077a
Dylan Weijgertze <dylanwprivate@gmail.com>
2022-05-28 12:33:06 +0200

    removed laravel logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,8 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('home') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                    <a class="text-red-500 font-bold" href="{{ route('home') }}">
+                        Ho-wan-loi
                     </a>
                 </div>
 
