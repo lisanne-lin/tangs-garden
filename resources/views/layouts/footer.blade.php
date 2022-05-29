590895324c3683f55e4d202e5c940665afbf022a
Dylan Weijgertze <dylanwprivate@gmail.com>
2022-05-29 12:01:59 +0200

    added links and phonenumber in env

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
         <span class="text-sm text-gray-500 sm:text-center">© {{ date("Y") . ' ' . config('app.name', 'Ho Wan Loi') }}</span>
         <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
             <li>
-                <a href="/" class="mr-4 hover:underline md:mr-6">home</a>
+                <a href="/" class="mr-4 hover:underline md:mr-6">Home</a>
             </li>
             <li>
                 <a href="/menu" class="mr-4 hover:underline md:mr-6">Menu</a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,13 +25,13 @@
                         Erasmusplein 10, 4834 AD Breda
                     </p>
                     <p>
-                        <a href="#" class="rounded-full bg-red-500 p-4 px-6 text-white hover:bg-red-600 transition-colors">
+                        <a href="tel:{{ env('USER_PHONE') }}" class="rounded-full bg-red-500 p-4 px-6 text-white hover:bg-red-600 transition-colors">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                               </svg>
                             Bel nu
                         </a>
-                        <a href="#" class="ml-4 text-red-500">
+                        <a href="/menu" class="ml-4 text-red-500">
                             Naar menu
                         </a>
                     </p>
